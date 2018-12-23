6d57ab1688a5e6565e5c3525655b23d0f22a3188
Tal <tal-vr@hotmail.com>
2018-12-23 18:47:00 +0200

    added message to returnJson

--- Response.js
+++ Response.js
@@ -2,25 +2,33 @@
 
 module.exports = class RestResponse{
     /**
-     * 
+     * @param {number} statusCode
      * @param {http.ServerResponse} res 
      */
-    constructor(res){
+    constructor(res,statusCode){
         response = res;
+        this.statusCode = statuscode;
     }
 
     /**
      * 
      * @param {JSON|Array} body 
+     * @param {string} message
      * @param {Number} statusCode - Default 200
      * @param {Number} code - default 0
      * @returns Return the Object\Array as JSON with Status and Code
      */
-    returnJson(body,statusCode=200,code=0){
+    returnJson(body,message,statusCode=null,code=0){
         
         if(typeof body == "object" || body instanceof Array){
-            response.statusCode = statusCode;
-            let obj = {code,data:body,statusCode};
+            if(statusCode != null){
+                response.statusCode = statusCode;
+            }else if(this.statusCode != null){
+                statusCode = this.statusCode;
+            }else{
+                statusCode = 200;
+            }
+            let obj = {code,data:body,message};
             response.write(JSON.stringify(obj));
         }else{
             throw new Error("Body Data not typeof Object/Array");
